42c9b866d8fbf992b8c97771eda7afce52179495
tylerwebster <webstertyler81@yahoo.com>
2024-05-02 15:44:18 -0400

    error handling finished

--- aa04-error-handling/error-handling.js
+++ aa04-error-handling/error-handling.js
@@ -7,14 +7,43 @@ function sum(array) {
   return sum;
 }
 
-let res = sum(null);
-console.log(res);
+
+try {
+  let res = sum(null);
+  console.log(res);
+} catch (error) {
+  if (error instanceof TypeError) {
+    console.error(error.name)
+  } else {
+    console.error('error')
+  }
+}
+
 
 // 2.
 // tests
-sayName("Alex");
-sayName(1);
-// Your code here 
+function sayName(name) {
+  if (typeof name !== 'string') {
+    // when referncing the error objects error.name is === to error type,
+    // error.message === manual message we gave our typeerror
+    throw new TypeError("Invalid name! Must be a string!")
+  } else {
+    console.log(name)
+  }
+}
+try {
+  sayName("Alex");
+  sayName(1);
+} catch (error) {
+  if (error instanceof TypeError) {
+    console.error(error.message)
+  } else {
+    console.error('error')
+  }
+
+}
+
+// Your code here
 
 // 3.
 function greet(greeting) {
@@ -25,3 +54,8 @@ function greet(greeting) {
   console.log(greeting);
 }
 
+try{
+greet()
+}catch (e){
+ console.error('hello world')
+}
